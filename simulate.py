4016135cb32c8663960bb6631bcd0d41658c6889
Julia Sober <sober.julia@gmail.com>
2025-01-15 18:25:03 -0500

    updating simulate to include floor and gravity

--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+planeId = p.loadURDF("plane.urdf")
+p.setGravity(0, 0, -9.8)
 p.loadSDF("box.sdf")
+
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
