0274c6ee94f5cf256b07d72a924e14bb8fbf7fe6
Julia Sober <sober.julia@gmail.com>
2025-01-15 19:46:04 -0500

    creating the towers

--- generate.py
+++ generate.py
@@ -1,5 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 0.5
+
+pyrosim.Start_SDF("boxes.sdf")
+
+# loops for rows and columns of towers
+for k in range(5):
+    for j in range(5):
+        # loop for tower height
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x+k,y+j,z+i] , size=[width*0.9**i,length*0.9**i,height*0.9**i])
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 planeId = p.loadURDF("plane.urdf")
 p.setGravity(0, 0, -9.8)
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
