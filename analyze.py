7be56c9eac49385ef366d76f12693d63fe0f3e08
Julia Sober <sober.julia@gmail.com>
2025-02-05 16:57:29 -0500

    Analyzing

---
+++ analyze.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+backLegSensorValues = np.load("data/backLegSensorData.npy")
+frontLegSensorValues = np.load("data/frontLegSensorData.npy")
+plt.plot(backLegSensorValues, label="Back Leg", linewidth=3)
+plt.plot(frontLegSensorValues, label="Front Leg")
+plt.legend()
+plt.show()
--- simulate.py
+++ simulate.py
@@ -15,12 +15,15 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     # print(f"Iteration {i+1}")
     time.sleep(1/60)
 
 p.disconnect()
-np.save("data/sensor_data.npy", backLegSensorValues)
+np.save("data/backLegSensorData.npy", backLegSensorValues)
+np.save("data/frontLegSensorData.npy", frontLegSensorValues)
